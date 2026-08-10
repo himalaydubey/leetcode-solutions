b56007322e955535bf9d2f6317ca7640547a128f
himalaydubey <himalaydubey4@gmail.com>
2026-08-10 19:53:25 +0530

    Create Palindrome Number.java

---
+++ Palindrome Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        int temp=x;
+        int rev=0,rem;
+        while(temp!=0){
+            rem=temp%10;
+            rev=rev*10+rem;
+            temp=temp/10;
+        }
+        return x==rev;
+    }
+}
