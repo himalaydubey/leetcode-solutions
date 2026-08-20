da76489b8ff721cce516ed1436ad74a50709bf71
Himalay Dubey <himalaydubey4@gmail.com>
2026-08-20 16:39:54 +0530

    Create 3005. Count Elements With Maximum Frequency.java

---
+++ 3005. Count Elements With Maximum Frequency.java
@@ -0,0 +1,29 @@
+    public int maxFrequencyElements(int[] nums) {
+        int max = 0;
+        for(int i=0; i<nums.length; i++){
+            int count = 0;
+            for(int j=0; j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count>max){
+                max = count;
+            }
+        }
+             int ans = 0;
+             for(int i=0; i<nums.length; i++){
+                int count = 0;
+                for(int j=0; j<nums.length; j++){
+                    if(nums[i]==nums[j]){
+                        count++;
+                    }
+                }
+             if(count==max){
+                ans++;
+             } 
+        }
+        return ans;
+        
+    }
+}
