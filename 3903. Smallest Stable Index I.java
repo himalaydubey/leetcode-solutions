d7866ed93d9d66d379dc007955e3c009191b7754
Himalay Dubey <himalaydubey4@gmail.com>
2026-09-04 22:39:42 +0530

    Create 3903. Smallest Stable Index I.java

---
+++ 3903. Smallest Stable Index I.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+       int max = Integer.MIN_VALUE;
+       int min = Integer.MAX_VALUE;
+       for(int i=0; i<nums.length; i++){
+        if(nums[i]>max){
+            max = nums[i];
+        }
+        min = Integer.MAX_VALUE;
+
+            for(int j = i; j < nums.length; j++){
+                if(nums[j] < min){
+                    min = nums[j];
+                }
+            }
+                int b=max-min;
+                if(b<=k){
+                    return i;
+                }
+            }
+            return -1;
+            
+    }
+}
